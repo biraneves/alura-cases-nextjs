5d3b055af730e04346c29dce70158ea4b0b9866f
Ubirajara Neves <ubirajara.neves@usp.br>
2022-05-17 14:43:42 -0300

    Arquivos de estilização

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import GlobalStyle from "../src/theme/GlobalStyles";
+
+function MyApp({Component, pageProps}) {
+
+    return (
+        <>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    );
+
+}
+
+export default MyApp;
